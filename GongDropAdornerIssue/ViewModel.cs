d3b9de40757ad98fba4e82165a05c7fa7e63f1be
brather1ng <github@soeren-thomas.de>
2017-03-01 21:43:37 +0100

    Add content

---
+++ GongDropAdornerIssue/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using GongSolutions.Wpf.DragDrop;
+
+namespace GongDropAdornerIssue
+{
+    public class ViewModel : IDragSource
+    {
+        public ImageSource Image { get; }
+            = new BitmapImage(new Uri("94x94.png", UriKind.RelativeOrAbsolute));
+
+        public void StartDrag(IDragInfo dragInfo)
+        {
+            dragInfo.Data = this;
+            dragInfo.Effects = DragDropEffects.Move;
+        }
+
+        public bool CanStartDrag(IDragInfo dragInfo)
+            => true;
+
+        public void Dropped(IDropInfo dropInfo)
+        {
+        }
+
+        public void DragCancelled()
+        {
+        }
+
+        public bool TryCatchOccurredException(Exception exception)
+            => false;
+    }
+}
